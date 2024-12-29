9b1abded56d8d7e86db8907239c9678e766e92cd
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-29 10:12:48 +0100

    added

---
+++ Swift-Animated-Text/Swift-Animated-Text/SceneDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SceneDelegate.swift
+//  Swift-Animated-Text
+//
+//  Created by Mac on 02/12/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .white
+        let rootViewController = ViewController()
+        window?.rootViewController = rootViewController
+        let customColor = #colorLiteral(red: 0.5568627451, green: 0.4235294118, blue: 0.937254902, alpha: 1)
+        window?.backgroundColor = customColor
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
+
