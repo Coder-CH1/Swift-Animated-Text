c2d5c737ea1e714c770779b0fba390edbb06239c
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-29 19:06:50 +0100

    added

--- Swift-Animated-Text/Swift-Animated-Text/ViewController.swift
+++ Swift-Animated-Text/Swift-Animated-Text/ViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 //MARK: - UI -
 class ViewController: UIViewController {
-    
+
     //MARK: - LABEL -
-    fileprivate lazy var animatedLabel: UILabel = {
+    fileprivate lazy var firstAnimatedLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .white
@@ -19,24 +19,28 @@ class ViewController: UIViewController {
         label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
         return label
     }()
-
+    
+    //MARK: - LABEL -
+    var secondAnimatedLabels: [UILabel] = []
+    
     //MARK: - LIFE CYCLE -
     override func viewDidLoad() {
         super.viewDidLoad()
-        animateLabel(text: "I Love UIKit...")
+        animateFirstLabel(text: "I Love UIKit...")
+        animateSecondLabel(text: "I Love UIKit so much...")
     }
-    
-    //MARK: - METHOD TO ANIMATE LABEL -
-    func animateLabel(text label: String) {
+
+    //MARK: - METHOD TO ANIMATE FIRST LABEL -
+    func animateFirstLabel(text label: String) {
         var currentIndex = 0
         let lenght = label.count
-        
-        animatedLabel.text = ""
-        
+
+        firstAnimatedLabel.text = ""
+
         Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { timer in
             if currentIndex < lenght {
                 let index = label.index(label.startIndex, offsetBy: currentIndex)
-                self.animatedLabel.text?.append(label[index])
+                self.firstAnimatedLabel.text?.append(label[index])
                 currentIndex += 1
             } else {
                 timer.invalidate()
@@ -44,13 +48,36 @@ class ViewController: UIViewController {
         }
         setSubviewsAndLayout()
     }
-   
+    
+    //MARK: - METHOD TO ANIMATE FIRST LABEL -
+    func animateSecondLabel(text label: String) {
+        let width: CGFloat = 30
+        let height: CGFloat = 40
+        let startY: CGFloat = -50
+        
+        for (index, character) in label.enumerated() {
+            let lbl = UILabel()
+            lbl.text = String(character)
+            lbl.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+            lbl.textColor = .white
+            lbl.frame = CGRect(x: CGFloat(50 + index * Int(width)), y: startY, width: width, height: height)
+            
+            self.view.addSubview(lbl)
+            
+            secondAnimatedLabels.append(lbl)
+            
+            UIView.animate(withDuration: 0.7, delay: Double(index) * 0.1,options: .curveEaseOut) {
+                lbl.frame.origin.y = CGFloat(200 + index * Int(height))
+            }
+        }
+    }
+
     // MARK: - Subviews and Layout -
     func setSubviewsAndLayout() {
-        view.addSubview(animatedLabel)
+        view.addSubview(firstAnimatedLabel)
         NSLayoutConstraint.activate([
-            animatedLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            animatedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstAnimatedLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            firstAnimatedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
 }
