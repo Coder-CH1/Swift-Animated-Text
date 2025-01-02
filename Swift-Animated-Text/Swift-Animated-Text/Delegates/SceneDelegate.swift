49fc0afbf0fe0adcb4a63dc46d2657d79c773bb2
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2025-01-02 11:37:51 +0100

    added

--- Swift-Animated-Text/Swift-Animated-Text/Delegates/SceneDelegate.swift
+++ Swift-Animated-Text/Swift-Animated-Text/Delegates/SceneDelegate.swift
@@ -20,8 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let rootViewController = ViewController()
         window?.rootViewController = rootViewController
         
-        let colors: [UIColor] = [#colorLiteral(red: 0.3952487111, green: 0.07191131264, blue: 0.09358521551, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.03250751629, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2949430026, green: 0.06731580187, blue: 0.03667572662, alpha: 1), #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1), #colorLiteral(red: 0.008313905943, green: 0.2409412944, blue: 0.2374770158, alpha: 1)]
-        window?.animateColorsSequence(colors: colors, duration: 1.5)
+        let colors: [UIColor] = [#colorLiteral(red: 0.3952487111, green: 0.07191131264, blue: 0.09358521551, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.03250751629, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2949430026, green: 0.06731580187, blue: 0.03667572662, alpha: 1), #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1), #colorLiteral(red: 0.008313905943, green: 0.2409412944, blue: 0.2374770158, alpha: 1), #colorLiteral(red: 0.09688199984, green: 0.1477390103, blue: 0.686814635, alpha: 1)]
+        window?.animateColorsSequence(colors: colors, duration: 1.0)
         window?.makeKeyAndVisible()
     }
     
