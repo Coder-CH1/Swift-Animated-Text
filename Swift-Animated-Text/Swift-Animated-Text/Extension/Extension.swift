9851f72a8295dc99f68ccd0c852581504920b464
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2025-01-02 13:47:50 +0100

    added

--- Swift-Animated-Text/Swift-Animated-Text/Extension/Extension.swift
+++ Swift-Animated-Text/Swift-Animated-Text/Extension/Extension.swift
@@ -7,23 +7,6 @@
 
 import UIKit
 
-//MARK: - UIColor extension for blending/mixing array of colors -
-extension UIColor {
-    static func blend(colors: [UIColor]) -> UIColor {
-        let numberOfColors = CGFloat(colors.count)
-        var (red, green, blue, alpha) = (CGFloat(0), CGFloat(0), CGFloat(0), CGFloat(0))
-        
-        let componentsSum = colors.reduce((red: CGFloat(0), green: CGFloat(0), blue: CGFloat(0), alpha: CGFloat())) { temp, color in
-            color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-            return (temp.red+red, temp.green + green, temp.blue + blue, temp.alpha+alpha)
-        }
-        return UIColor(red: componentsSum.red / numberOfColors,
-                       green: componentsSum.green / numberOfColors,
-                       blue: componentsSum.blue / numberOfColors,
-                       alpha: componentsSum.alpha / numberOfColors)
-    }
-}
-
 //MARK: - Extension for animating background colors sequentially -
 extension UIView {
     func animateBackgroundColor(to color: UIColor, duration: TimeInterval, completion: (() -> Void)? = nil) {
